bc2fe6e9db4820cf6cf083a0ac946df4199a010c
Brandon Matthews <bmatt@optimaltour.us>
2014-07-14 19:39:49 -0700

    Changed setup script to be useful to everyone else

--- setup-rtems.sh
+++ setup-rtems.sh
@@ -1,62 +1,21 @@
 #!/bin/bash
 
-BUILD_BRANCH=bbxm
-TOOLS_BRANCH=bbxm-wip
-RTEMS_BRANCH=beaglebone-wip
+set -e
 
-enosys() {
-    local distro=$1
-    if [[ "$distro" != "" ]]; then
-        echo "Sorry, this script hasn't been extended to include $distro."
-    else
-        echo "Sorry, this script hasn't been extended to include this platform."
-    fi
-    exit 1
-}
+BUILD_BRANCH=master
+TOOLS_BRANCH=master
+RTEMS_BRANCH=master
 
-get_buildhost() {
-    ostype=$(uname -s)
+DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y git
+apt-get build-dep -y binutils gcc g++ gdb unzip git python2.7-dev
 
-    case $ostype in
-    "Linux")
-        has_lsbr=$(lsb_release >/dev/null 2>&1)
-        if [ $? -eq 0 ]; then
-            distro=$(lsb_release -si)
-            if [[ $distro == "Ubuntu" ]]; then
-                echo "$distro"
-            else
-                enosys $distro
-            fi
-        else
-            enosys "Linuxes without lsb_release"
-        fi
-    ;;
-    *)
-        enosys $ostype
-    ;;
-    esac
-}
+vagrant git clone -b "${BUILD_BRANCH}" https://github.com/bengras/rtems-source-builder.git 
+vagrant git clone -b "${TOOLS_BRANCH}" https://github.com/bengras/rtems-tools.git
+vagrant git clone -b "${RTEMS_BRANCH}" https://github.com/bengras/rtems.git
 
-buildhost=$(get_buildhost)
-case "$buildhost" in
-    Ubuntu)
-        DEBIAN_FRONTEND=noninteractive
-        apt-get update
-        apt-get install -y git
-        apt-get build-dep -y binutils gcc g++ gdb unzip git python2.7-dev
-        if [ $? -ne 0 ]; then
-            exit 1
-        fi
-    ;;
-    *)
-        enosys
-    ;;
-esac
-
-
-sudo -u vagrant git clone -b "${BUILD_BRANCH}" https://github.com/bengras/rtems-source-builder.git 
-sudo -u vagrant git clone -b "${TOOLS_BRANCH}" https://github.com/bengras/rtems-tools.git
-sudo -u vagrant git clone -b "${RTEMS_BRANCH}" https://github.com/bengras/rtems.git
+chown vagrant:vagrant *
 
 # sb-check returns 0 on failed check (but without fatal error), so work around
 isok=$(./rtems-source-builder/source-builder/sb-check | grep -c "Environment is ok")
