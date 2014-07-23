86790c026b65a477800f8e87c35e58f1112c15ce
Brandon Matthews <bmatt@optimaltour.us>
2014-07-23 16:04:07 -0700

    tweak and add tests

--- setup-rtems.sh
+++ setup-rtems.sh
@@ -23,6 +23,7 @@ echo "Checking build environment..."
 # sb-check returns 0 on failed check (but without fatal error), so ignore the output
 ./rtems-source-builder/source-builder/sb-check
 
+echo "Building source tools..."
 cd rtems-source-builder/rtems
 ../source-builder/sb-set-builder \
     --log=beagle.txt \
@@ -41,14 +42,39 @@ cd rtems-src
 
 echo "Building RTEMS..."
 mkdir /home/vagrant/development/rtems/b-beagle; cd /home/vagrant/development/rtems/b-beagle
-CONSOLE_POLLED=1 /home/vagrant/development/rtems-src/sources/rtems/configure \
+CONSOLE_POLLED=1 /home/vagrant/development/rtems/sources/rtems-src/configure \
     --target=arm-rtems4.11 \
     --enable-rtemsbsp="beagleboneblack beagleboardxm" \
     --enable-tests
 make
 
+if [ $? != 0 ]; then
+    echo "Make failed!"
+    exit 1
+fi
+
+echo "Building a hello-world SD card image..."
+cd /home/vagrant/development/rtems/rtems-src/c/src/lib/libbsp/arm/beagle/simscripts
+sh sdcard.sh \
+    /home/vagrant/development/rtems/4.11 \
+    /home/vagrant/development/rtems/b-beagle/arm-rtems4.11/c/beagleboneblack/testsuites/samples/hello/hello.exe
+if [[ ! -e bone_hello.exe-sdcard.img ]]; then
+    echo "Failed to build SD card image."
+    exit 1
+fi
+cp bone_hello.exe-sdcard.img /vagrant/
+
 echo "Getting testing tools..."
 cd /home/vagrant/development/rtems/
 git clone -b "${TOOLS_BRANCH}" ${GIT_ROOT}/rtems-tools.git || exit 1
+
+echo "Running tests..."
+cd rtems-tools/tester
+./rtems-test \
+    --log=bbxm.log \
+    --report-mode=all \
+    --rtems-bsp=beagleboardxm_qemu \
+    --rtems-tools=/home/vagrant/development/rtems/4.11 \
+    /home/vagrant/development/rtems/b-beagle/arm-rtems4.11/c/beagleboardxm
 
 chown -R vagrant:vagrant /home/vagrant/
