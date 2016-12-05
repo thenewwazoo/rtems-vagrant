b7cc23543e6d9b6cfc6a878b616d48f4a21c0d41
Brandon Matthews <bmatt@optimaltour.us>
2016-12-04 21:15:42 -0800

    Archival cleanup commit

--- Vagrantfile
+++ Vagrantfile
@@ -4,14 +4,37 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/precise64"
 
-  config.vm.provider "virtualbox" do |vb|
-    vb.name = "rtems-autobuild"
-    vb.cpus = 4 # Higher is better
-    vb.customize ["modifyvm", :id, "--memory", "2048"]
+  config.vm.box = "dummy"
+  config.vm.box_url = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
+
+  config.vm.provider :aws do |aws, override|
+    aws.access_key_id = ENV["AWS_ACCESS_KEY_ID"]
+    aws.secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+
+    aws.ami = "ami-3d2cce5d"
+    aws.instance_type = "m4.2xlarge"
+    aws.security_groups = ["default",]
+    aws.region = "us-west-2"
+
+    aws.keypair_name = "aws"
+    override.ssh.username = "ubuntu"
+    override.ssh.private_key_path = ENV["HOME"] + "/.ssh/aws"
+
+    aws.block_device_mapping = [{ 'DeviceName' => '/dev/sda1', 'Ebs.VolumeSize' => 50 }]
+
+    aws.tags = {
+      'Name' => 'rtems-vagrant'
+    }
+
   end
 
-  config.vm.provision "shell", path: "setup-rtems.sh"
+#  config.vm.provider "virtualbox" do |vb|
+#    vb.name = "rtems-autobuild"
+#    vb.cpus = 4 # Higher is better
+#    vb.customize ["modifyvm", :id, "--memory", "2048"]
+#  end
+
+#  config.vm.provision "shell", path: "setup-rtems.sh"
 
 end
--- setup-rtems.sh
+++ setup-rtems.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 
-RTEMS_BRANCH=master
+RTEMS_BRANCH=4.11
 BUILD_BRANCH=beagle
 
 RTEMS_ROOT="https://github.com/rtems"
 BUILD_ROOT="https://github.com/bengras"
 
+ENV_ROOT=$(pwd)
+LOGIN_USER=$(whoami)
 echo "Updating host environment..."
 DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y git
+apt-get install -y git python2.7-dev
 apt-get build-dep -y binutils gcc g++ gdb unzip git python2.7-dev
 
-cd /home/vagrant/
+cd "${ENV_ROOT}"
 
 mkdir -p development/rtems/sources ; cd development/rtems/sources
 
 echo "Checking out source builder..."
-git clone -b "${BUILD_BRANCH}" ${BUILD_ROOT}/rtems-source-builder.git  || exit 1
+if [ -d "rtems-source-builder" ]
+then
+    cd rtems-source-builder && git pull && cd ..
+else
+    git clone -b "${BUILD_BRANCH}" ${BUILD_ROOT}/rtems-source-builder.git  || exit 1
+fi
+
+cd rtems-source-builder
+git checkout 1d441d0184c72c3e313bc70df9cc2e284c0cb360
+cd ..
+
+find . -name qemu-1-1.cfg -exec sed '/disable-opengl/d' {} \;
 
 echo "Checking build environment..."
 # sb-check returns 0 on failed check (but without fatal error), so ignore the output
-./rtems-source-builder/source-builder/sb-check
+#./rtems-source-builder/source-builder/sb-check
 
 echo "Building tools..."
 cd rtems-source-builder/rtems
 ../source-builder/sb-set-builder \
     --log=beagle.txt \
-    --prefix=/home/vagrant/development/rtems/4.11 \
-    devel/beagle.bset
+    --prefix=${ENV_ROOT}/development/rtems/4.11 \
+    devel/beagle.bset || exit 1
 
-export PATH=/home/vagrant/development/rtems/4.11/bin:$PATH
-cd /home/vagrant/development/rtems/
+export PATH=/development/rtems/4.11/bin:$PATH
+cd ${ENV_ROOT}/development/rtems/
 
 echo "Checkout out RTEMS source..."
-git clone -b "${RTEMS_BRANCH}" ${RTEMS_ROOT}/rtems.git rtems-src || exit 1
+if [ -d "rtems-src" ]
+then
+    cd rtems-src && git pull && cd ..
+else
+    git clone -b "${RTEMS_BRANCH}" ${RTEMS_ROOT}/rtems.git rtems-src || exit 1
+fi
 
 echo "Bootstrapping RTEMS..."
 cd rtems-src
-./bootstrap ; ./bootstrap -p
+./bootstrap || exit 1
+./bootstrap -p || exit 1
 
 echo "Building RTEMS..."
-mkdir /home/vagrant/development/rtems/b-beagle; cd /home/vagrant/development/rtems/b-beagle
-CONSOLE_POLLED=1 /home/vagrant/development/rtems/rtems-src/configure \
+mkdir ${ENV_ROOT}/development/rtems/b-beagle; cd ${ENV_ROOT}/development/rtems/b-beagle
+CONSOLE_POLLED=1 ${ENV_ROOT}/development/rtems/rtems-src/configure \
     --target=arm-rtems4.11 \
-    --enable-rtemsbsp="beagleboneblack beagleboardxm"
+    --enable-rtemsbsp="beagleboneblack beagleboardxm" || exit 1
 make
 
 if [ $? != 0 ]; then
@@ -59,14 +78,14 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-echo <<EOF > ~vagrant/.bash_profile
-export RTEMS_PREFIX=/home/vagrant/development/rtems/4.11
+echo <<EOF > ~${LOGIN_USER}/.bash_profile
+export RTEMS_PREFIX=${ENV_ROOT}/development/rtems/4.11
 export RTEMS_MAKEFILE_PATH=/opt/rtems-4.11/arm-rtems4.11/beagleboneblack
-export PATH=/home/vagrant/development/rtems/4.11/bin:$PATH
+export PATH=${ENV_ROOT}/development/rtems/4.11/bin:$PATH
 
-alias go="make && cd ~/build_output/ && /home/vagrant/development/rtems/rtems-src/c/src/lib/libbsp/arm/beagle/simscripts/sdcard.sh $RTEMS_PREFIX ./beaglebone.exe && cp bone_beaglebone.exe-sdcard.img /vagrant/beaglebone-test1/ && cd -"
+alias go="make && cd ~/build_output/ && ${ENV_ROOT}/development/rtems/rtems-src/c/src/lib/libbsp/arm/beagle/simscripts/sdcard.sh $RTEMS_PREFIX ./beaglebone.exe && cp bone_beaglebone.exe-sdcard.img ${LOGIN_USER}/beaglebone-test1/ && cd -"
 EOF
 
-echo 'Be sure to make a symlink from o-optimize to /home/vagrant/build_output/'
-mkdir /home/vagrant/build_output
-chown -R vagrant:vagrant /home/vagrant/
+echo 'Be sure to make a symlink from o-optimize to ${ENV_ROOT}/build_output/'
+mkdir ${ENV_ROOT}/build_output
+chown -R ${LOGIN_USER}:${LOGIN_USER} "${ENV_ROOT}"
